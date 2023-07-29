e690e0928d4f5fb821332bc30ec041ccc662fe6a
Hugues Demers <hdemers@gmail.com>
2023-07-29 17:29:41 -0400

    Various small updates

--- .shellrc.d.symlink/00-env.sh
+++ .shellrc.d.symlink/00-env.sh
@@ -5,6 +5,8 @@ export EDITOR=/usr/bin/vim
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/local/bin:$PATH
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export PATH=$HOME/.cargo/bin:$PATH
+
 export CPATH=$CPATH:$HOME/.local/include
 export LIBRARY_PATH=$LIBRARY_PATH:$HOME/.local/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib

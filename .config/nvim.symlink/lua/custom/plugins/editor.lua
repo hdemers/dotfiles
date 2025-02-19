4fc251c5fdc5e146b09422149480f1a35cc03432
Hugues Demers <hdemers@gmail.com>
2025-02-19 15:04:40 -0500

    feat: more Snacks

--- .config/nvim.symlink/lua/custom/plugins/editor.lua
+++ .config/nvim.symlink/lua/custom/plugins/editor.lua
@@ -362,9 +362,11 @@ return {
       -- this will change the font size on alacritty when in zen mode
       -- requires  Alacritty Version 0.10.0 or higher
       -- uses `alacritty msg` subcommand to change font size
-      alacritty = {
-        enabled = true,
-        font = '16', -- font size
+      plugins = {
+        wezterm = {
+          enabled = true,
+          font = '+2', -- font size
+        },
       },
     },
   },
@@ -831,20 +833,43 @@ return {
         desc = '[g]o to [d]efinition',
       },
     },
-    config = function(_, opts)
-      local snacks = require 'snacks'
-      snacks.setup(opts)
+    init = function()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'VeryLazy',
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-      local indent_on = false
-
-      vim.keymap.set('n', '<leader>ci', function()
-        if indent_on then
-          Snacks.indent.disable()
-        else
-          Snacks.indent.enable()
-        end
-        indent_on = not indent_on
-      end, { desc = '[c]ode [i]ndent' })
+          -- Create some toggle mappings
+          Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
+          Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+          Snacks.toggle
+            .option('relativenumber', { name = 'Relative Number' })
+            :map '<leader>uL'
+          Snacks.toggle.diagnostics():map '<leader>ud'
+          Snacks.toggle.line_number():map '<leader>ul'
+          Snacks.toggle
+            .option(
+              'conceallevel',
+              { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }
+            )
+            :map '<leader>uc'
+          Snacks.toggle.treesitter():map '<leader>uT'
+          Snacks.toggle
+            .option('background', { off = 'light', on = 'dark', name = 'Dark Background' })
+            :map '<leader>ub'
+          Snacks.toggle.inlay_hints():map '<leader>uh'
+          Snacks.toggle.indent():map '<leader>ug'
+          Snacks.toggle.dim():map '<leader>uD'
+          Snacks.toggle.zen():map '<leader>uz'
+        end,
+      })
     end,
   },
 }

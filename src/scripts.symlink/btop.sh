3ce3265d3cc6538dc57e79e91f8c30ed90a5101e
Hugues Demers <hdemers@gmail.com>
2025-03-20 08:52:40 -0400

    chore: update btop version in install script

--- src/scripts.symlink/btop.sh
+++ src/scripts.symlink/btop.sh
@@ -3,9 +3,9 @@ cd $HOME/src
 # Check the arch with uname -m
 
 if [ "$(uname -m)" == "x86_64" ]; then
-	curl -LsS https://github.com/aristocratos/btop/releases/download/v1.2.13/btop-x86_64-linux-musl.tbz -o btop.tbz
+	curl -LsS https://github.com/aristocratos/btop/releases/download/v1.4.0/btop-x86_64-linux-musl.tbz -o btop.tbz
 elif [ "$(uname -m)" == "aarch64" ]; then
-	curl -LsS https://github.com/aristocratos/btop/releases/download/v1.2.13/btop-aarch64-linux-musl.tbz -o btop.tbz
+	curl -LsS https://github.com/aristocratos/btop/releases/download/v1.4.0/btop-aarch64-linux-musl.tbz -o btop.tbz
 else
 	echo "btop binary architecture not implemented."
 	exit 1

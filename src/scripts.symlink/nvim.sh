f873204ce38ac918adc976ec70cfef5c4f9fd01c
Hugues Demers <hdemers@gmail.com>
2025-04-04 15:04:40 -0400

    chore: modify src/scripts/nvim.sh to install aarch64 version

--- src/scripts.symlink/nvim.sh
+++ src/scripts.symlink/nvim.sh
@@ -1,8 +1,17 @@
 
 cd $HOME/src
-curl -LsSO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-tar xfz nvim-linux64.tar.gz
-ln -s $HOME/src/nvim-linux64/bin/nvim $HOME/.local/bin/nvim
+
+arch=$(uname -m)
+
+if [ "$arch" = "x86_64" ]; then
+    curl -LsSO https://github.com/neovim/neovim-releases/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
+    tar xfz nvim-linux-x86_64.tar.gz
+    ln -s $HOME/src/nvim-linux-x86_64/bin/nvim $HOME/.local/bin/nvim
+elif [ "$arch" = "aarch64" ]; then
+    curl -LsSO https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-arm64.tar.gz
+    tar xfz nvim-linux-aarch64.tar.gz
+    ln -s $HOME/src/nvim-linux-aarch64/bin/nvim $HOME/.local/bin/nvim
+fi
 
 unset PIP_REQUIRE_VIRTUALENV
 

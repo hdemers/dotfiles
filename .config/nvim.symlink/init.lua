c7d568e1d197565391335fa3bdb7dff0f235f025
Hugues Demers <hdemers@gmail.com>
2025-06-17 08:06:02 -0400

    feat: Update Neovim config and add AI plugins
    - Update servers.json.symlink for MCP server configuration.
    - Modify init.lua to incorporate new plugin setups.
    - Update lazy-lock.json due to plugin changes.
    - Enhance editor.lua with new editor-related features.
    - Introduce ai.lua for local-only AI plugin integration.

--- .config/nvim.symlink/init.lua
+++ .config/nvim.symlink/init.lua
@@ -1,88 +1,5 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know how the Neovim basics, you can skip this step)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not sure exactly what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or neovim features used in kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your nvim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
+-- ─────────────────────────────────────────────────────────────────────────────── --
+-- [[ General Neovim Settings ]]
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
@@ -165,9 +82,24 @@ vim.opt.termguicolors = true
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
--- Sets the Python executable to be used by Neovim. This requires the existence of a
--- virtualenv named `nvim` with packages `pynvim` and `jupyter_client` installed.
+-- Treesitter-based folding.
+-- Don't enable folding by default
+vim.opt.foldenable = false
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldtext = ''
+vim.opt.fillchars = {
+  foldclose = '>',
+  foldopen = '',
+}
+
+-- Sets the Python executable to be used by Neovim. This requires the existence
+-- of a virtualenv named `nvim` with packages `pynvim` and `jupyter_client`
+-- installed.
 vim.g.python3_host_prog = vim.env.HOME .. '/.virtualenvs/nvim/bin/python'
+
+-- ┌────────────────────────────────────────────────────────────────────────────
+-- │ Keymaps
 
 -- Clear highlight on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -244,18 +176,8 @@ vim.keymap.set('n', '<leader>w', ':tabclose <CR>', { desc = 'Close tab' })
 vim.keymap.set({ 'n', 'v' }, '<C-s>', '<C-a>', { noremap = true, silent = true })
 vim.keymap.set({ 'n', 'v' }, 'g<C-s>', 'g<C-a>', { noremap = true, silent = true })
 
--- Treesitter-based folding.
--- Don't enable folding by default
-vim.opt.foldenable = false
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.opt.foldtext = ''
-vim.opt.fillchars = {
-  foldclose = '>',
-  foldopen = '',
-}
-
--- [[ Basic Autocommands ]]
+-- ─────────────────────────────────────────────────────────────────────────────
+--  Basic Autocommands
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
@@ -290,7 +212,22 @@ vim.filetype.add {
   },
 }
 
+-- Auto-reload
+vim.go.autoread = true
+vim.api.nvim_create_autocmd({ 'FocusGained', 'CursorHold', 'CursorHoldI', 'BufEnter' }, {
+  desc = 'Reload files when focus is gained',
+  group = vim.api.nvim_create_augroup('kickstart-auto-reload', { clear = true }),
+  callback = function()
+    if vim.bo.modified then
+      return
+    end
+    vim.cmd 'checktime'
+  end,
+})
+
+-- ─────────────────────────────────────────────────────────────────────────────
 -- [[ Install `lazy.nvim` plugin manager ]]
+
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then

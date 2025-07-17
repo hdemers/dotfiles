7f87cc4a248c7b951a4bc88e8e3de009a1c28dba
Hugues Demers <hdemers@gmail.com>
2025-07-17 07:46:24 -0400

    feat(shell): improve shell functions

--- .shellrc.d.symlink/03-functions.sh
+++ .shellrc.d.symlink/03-functions.sh
@@ -617,7 +617,10 @@ jop() {
         --color='fg:#f8f8f2,bg:#282a36,hl:#bd93f9' \
         --color='fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9' \
         --color='info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6' \
-        --color='marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+        --color='marker:#ff79c6,spinner:#ffb86c,header:#6272a4' \
+        --border-label-pos 5:bottom \
+        --border 'rounded' \
+        --border-label ' ctrl-r: restore'
 }
 
 lsemr() {

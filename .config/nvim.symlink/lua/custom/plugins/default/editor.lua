3d9e8214faf813ec0125d2bce53b86354a06069a
Hugues Demers <hdemers@gmail.com>
2025-09-20 15:25:58 -0400

    fix(zellij): move focus or tab
    Remove the 'or tab'

--- .config/nvim.symlink/lua/custom/plugins/default/editor.lua
+++ .config/nvim.symlink/lua/custom/plugins/default/editor.lua
@@ -669,34 +669,4 @@ return {
       })
     end,
   },
-  {
-    -- Complement to https://github.com/hiasr/vim-zellij-navigator
-    'swaits/zellij-nav.nvim',
-    enabled = true,
-    lazy = true,
-    event = 'VeryLazy',
-    keys = {
-      {
-        '<a-h>',
-        '<cmd>ZellijNavigateLeftTab<cr>',
-        { silent = true, desc = 'navigate left or tab' },
-      },
-      {
-        '<a-j>',
-        '<cmd>ZellijNavigateDown<cr>',
-        { silent = true, desc = 'navigate down' },
-      },
-      {
-        '<a-k>',
-        '<cmd>ZellijNavigateUp<cr>',
-        { silent = true, desc = 'navigate up' },
-      },
-      {
-        '<a-l>',
-        '<cmd>ZellijNavigateRightTab<cr>',
-        { silent = true, desc = 'navigate right or tab' },
-      },
-    },
-    opts = {},
-  },
 }

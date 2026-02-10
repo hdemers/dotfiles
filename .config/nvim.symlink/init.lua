bbdae805a1cf77a57eae7df29e6417b64d7ef39e
Hugues Demers <hdemers@gmail.com>
2026-02-10 09:13:48 -0500

    ✨ feat(zellij): new stacked layouts
    Also, add neovim zellij plugin to move panes seamlessly between neovim
    and zellij.

--- .config/nvim.symlink/init.lua
+++ .config/nvim.symlink/init.lua
@@ -150,10 +150,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<A-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<A-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<A-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<A-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<A-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<A-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<A-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<A-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- Buffer related mappings
 -- Buffer zoom in/out
 vim.keymap.set(
--- .config/nvim.symlink/lua/custom/plugins/default/editor.lua
+++ .config/nvim.symlink/lua/custom/plugins/default/editor.lua
@@ -690,4 +690,32 @@ return {
       })
     end,
   },
+  {
+    'swaits/zellij-nav.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    keys = {
+      {
+        '<a-h>',
+        '<cmd>ZellijNavigateLeft<cr>',
+        { silent = true, desc = 'navigate left or tab' },
+      },
+      {
+        '<a-j>',
+        '<cmd>ZellijNavigateDown<cr>',
+        { silent = true, desc = 'navigate down' },
+      },
+      {
+        '<a-k>',
+        '<cmd>ZellijNavigateUp<cr>',
+        { silent = true, desc = 'navigate up' },
+      },
+      {
+        '<a-l>',
+        '<cmd>ZellijNavigateRight<cr>',
+        { silent = true, desc = 'navigate right or tab' },
+      },
+    },
+    opts = {},
+  },
 }
